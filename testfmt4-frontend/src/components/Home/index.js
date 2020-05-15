fe1592a77b1bd72134fbf710f998fa7d89422283
Kien Nguyen <kc97ble@gmail.com>
2020-05-15 13:23:20 +0800

    7

--- testfmt4-frontend/src/components/Home/index.js
+++ testfmt4-frontend/src/components/Home/index.js
@@ -5,6 +5,7 @@ import Form from "react-bootstrap/Form";
 import { useHistory } from "react-router-dom";
 
 import * as api from "../../api";
+import "./style.css";
 
 function TopBar() {
   return (
@@ -15,7 +16,7 @@ function TopBar() {
           src="logo192.png"
           width="30"
           height="30"
-          className="d-inline-block align-top"
+          className="d-inline-block align-top "
         />{" "}
         Test Formatter 4
       </Navbar.Brand>
@@ -23,9 +24,21 @@ function TopBar() {
   );
 }
 
+function get(obj, ...args) {
+  for (let i = 0; i < args.length; i++) {
+    if (obj == null) {
+      return null;
+    }
+    obj = obj[args[i]];
+  }
+  return obj;
+}
+
 function MainForm() {
   const [files, setFiles] = useState(null);
   const history = useHistory();
+  const fileName = get(files, 0, "name");
+
   return (
     <Form>
       <Form.File id="formcheck-api-custom" custom>
@@ -36,36 +49,54 @@ function MainForm() {
             setFiles(e.target.files);
           }}
         />
-        <Form.File.Label data-browse="Button text">
-          {JSON.stringify(files)}
+        <Form.File.Label data-browse="Choose file">
+          {fileName || ""}
         </Form.File.Label>
       </Form.File>
       <Button
         variant="primary"
+        className="mt-3"
+        block={true}
+        disabled={!fileName}
         onClick={async () => {
           const { uploaded_file_id: fileID } = await api.uploadFile(files[0]);
           history.push("/edit/" + fileID);
         }}
       >
-        Submit
+        Upload
       </Button>
     </Form>
   );
 }
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <TopBar />
-        <Container>
-          <Row>
-            <Col>
-              <MainForm />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+function Banner() {
+  return (
+    <div className="banner">
+      <h3 style={{ textAlign: "center" }}>Format your test suite</h3>
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <div>
+      <TopBar />
+      <Container
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          height: "calc(80vh - 100px)",
+          minHeight: "300px",
+        }}
+      >
+        <Row>
+          <Col>
+            <Banner />
+            <MainForm />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
